1b8fabd5b050bffa9b59eb99d44ae18e5f0de7dc
LucasZuk <lucas.zukervar@gmail.com>
2020-09-29 22:20:36 +0200

    HTML done

---
+++ functions.php
@@ -0,0 +1,21 @@
+﻿<? /************** RATING OBJECT ****************/
+
+class Rating {
+	private $rating_value;
+	private $rating_score;
+	
+	constructor() {
+		this.color = '';
+	}
+
+	public function publish_rating () 
+	{
+		echo for($i=1, $i<$rating_score, $i++){
+			'<img 	class=\'rating_star\' 
+					src=\'\/images\/icon-star.svg\'
+					alt=\'rating_star_image\' 
+			>
+			<p>' . $rating_value . '</p>'
+		}
+	}
+}
