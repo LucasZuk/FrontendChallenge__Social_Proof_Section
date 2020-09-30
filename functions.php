db91d4612f48551d38cd1d2c86333ebfa6f01a0f
LucasZuk <lucas.zukervar@gmail.com>
2020-09-30 09:50:57 +0200

    Top floor space OK

--- functions.php
+++ functions.php
@@ -1,21 +1,33 @@
-﻿<? /************** RATING OBJECT ****************/
+﻿<?php /************** RATING OBJECT ****************/
 
-class Rating {
-	private $rating_value;
-	private $rating_score;
-	
-	constructor() {
-		this.color = '';
-	}
+class Rating 
+{
+	private $_rating_value ="";
+	private $_rating_score="0";
 
 	public function publish_rating () 
 	{
-		echo for($i=1, $i<$rating_score, $i++){
-			'<img 	class=\'rating_star\' 
-					src=\'\/images\/icon-star.svg\'
-					alt=\'rating_star_image\' 
-			>
-			<p>' . $rating_value . '</p>'
-		}
+		 for($i=1; $i< $this->_rating_score; $i++)
+			{
+
+				echo 
+				'<img 	class=\'rating_star\' 
+						src=\'\.\/images\/icon-star.svg\'
+						alt=\'rating_star_image\' 
+				>';
+			}
+
+		echo '<p>' . $this->_rating_value . '</p>';
+		
+	}
+
+	public function set_rating_value($_rating_value)
+	{
+		$this->_rating_value = $_rating_value;
+	}
+
+	public function set_rating_score($_rating_score)
+	{
+		$this->_rating_score = $_rating_score;
 	}
 }
